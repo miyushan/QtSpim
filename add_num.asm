64a5768b6f379f2b2fad6b5f5e07ecda685c1b56
miyushan <smiushan2015@gmail.com>
2021-05-18 22:24:46 +0530

    code for addition and substitution is added

---
+++ add_num.asm
@@ -0,0 +1,10 @@
+# Start .text segment (program code)
+	.text
+	.globl main
+	
+main:
+	li $t0, 25
+	li $t1, 10
+	add $t2, $t0, $t1
+	li $v0, 10
+	syscall
---
+++ sub_num.asm
@@ -0,0 +1,10 @@
+# Start .text segment (program code)
+	.text
+	.globl main
+	
+main:
+	li $t0, 25
+	li $t1, 10
+	sub $t2, $t0, $t1
+	li $v0, 10
+	syscall
